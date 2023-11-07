82e02ae96be4937a5b0e521c76d9b3f69656e5b0
Skidway <craftskidway@gmail.com>
2023-11-07 21:54:30 +0100

    +I have no idea what im doing

--- src/main/java/com/para/PaRaApplication.java
+++ src/main/java/com/para/PaRaApplication.java
@@ -1,17 +1,33 @@
 package com.para;
 
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
 
 @SpringBootApplication
-@RestController
-public class PaRaApplication {
+@Log
+public class PaRaApplication implements CommandLineRunner {
+
+	private final DataSource dataSource;
+
+	public PaRaApplication(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaRaApplication.class, args);
-		//https://www.youtube.com/watch?v=Nv2DERaMx-4
+		//https://www.youtube.com/watch?v=Nv2DERaMx-4 1:33:00 ~
 		//Maraton for U <3
 	}
-
+	@Override
+	public void run(final String... args){
+		log.info("Datasource: "+ dataSource.toString());
+		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+		restTemplate.execute("select 1");
+	}
 }
