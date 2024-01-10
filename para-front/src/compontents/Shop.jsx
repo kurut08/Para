fb4b23ce5eb5c6b293c4e96aa2ac3a2e8e31e486
Mangekyou <jume7997@gmail.com>
2024-01-11 00:24:55 +0100

    Started Shop page

--- para-front/src/compontents/Shop.jsx
+++ para-front/src/compontents/Shop.jsx
@@ -1,8 +1,14 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
+import {Routes, Route, useNavigate} from 'react-router-dom';
+import {Toggle} from'./toggle/Toggle';
+import {Footer} from'./footer/Footer';
+import {PriceRangeBar} from './pricerangebar/PriceRangeBar'
+import './Shop.css';
 function Shop(){
     const defaultVaule = [];
     const [games, setGames] = useState(defaultVaule);
+    const navigate = useNavigate();
 
     const getGames = async () =>{
         try
@@ -19,22 +25,43 @@ function Shop(){
     };
     useEffect(() => {
         getGames();
-      }, []);    
+      }, []);
+
+    const navigateToHome = () => {
+        navigate('/');
+    };
     return(
         <div className="shop">
-          <h1>SHOP</h1>
-            {games.map((game) => (
-          <div className="item-container">
-            <p>Id: {game.id}</p>
-            <p>Title: {game.title} </p>
-            <p>Description: {game.description}</p>
-            <p>Price: {game.price} </p>
-            <p>ImageURL: {game.imageUrl} </p>
-            <p>Genres: {game.genres}</p>
-            <p>{'\n'}</p> 
-          </div>
-        ))}
+            <div className="logo-container">
+                <div onClick={navigateToHome}>
+                    <img src="/path/to/your/logo.png" alt="App Logo" className="app-logo"/>
+                </div>
+                <div className="switch-container">
+                    <Toggle/>
+                </div>
+            </div>
+            <div className="content-shop">
+                <h1>SHOP</h1>
+                <div className="sort">
+                    <PriceRangeBar/>
+                </div>
+                <div>
+                    {games.map((game) => (
+                        <div className="item-container">
+                            <p>Id: {game.id}</p>
+                            <p>Title: {game.title} </p>
+                            <p>Description: {game.description}</p>
+                            <p>Price: {game.price} </p>
+                            <p>ImageURL: {game.imageUrl} </p>
+                            <p>Genres: {game.genres}</p>
+                            <p>{'\n'}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <Footer/>
         </div>
     )
 }
+
 export default Shop;
